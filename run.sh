3f25b25ad5f1a415200c75e3fe3ca9a847bb1342
C. Titus Brown <titus@idyll.org>
2015-06-02 22:26:29 +0000

    fix mistake in bf size

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 bt hashes both.hash 1
-bt count both.hash 1e7 reads-a.fa filter-a.bf.bv
-bt count both.hash 1e7 reads-b.fa filter-b.bf.bv
+bt count both.hash 1000000 reads-a.fa filter-a.bf.bv
+bt count both.hash 1000000 reads-b.fa filter-b.bf.bv
 
 ls filter-?.bf.bv > filters-list.txt
 bt build both.hash filters-list.txt entire.bf.bv
